4eab2803e9f7a2b61c30aea86717c49ab3cf7c5f
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2017-11-07 16:54:10 +0100

    add first modul

---
+++ setup.ps1
@@ -0,0 +1,11 @@
+﻿# ============================================================
+# Powershell-Script zur Vorbereitung
+# Autor: Simon Bärlocher
+# ============================================================
+
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Exit
+}
+
+. ./modules/ConfigureRemotingForAnsible.ps1
