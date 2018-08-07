bbd30722c1e84f3c8e7e23bfc33b92302d3f9b9e
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2018-08-07 15:47:46 +0200

    test

--- setup.ps1
+++ setup.ps1
@@ -8,4 +8,5 @@ $file = "$env:TEMP\ConfigureRemotingForAnsible.ps1"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 
-powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -file $file' -verb RunAs}"
+
+powershell -noprofile -command "&{ start-process powershell -ArgumentList '-ExecutionPolicy Bypass -noprofile -file $file' -verb RunAs}"
